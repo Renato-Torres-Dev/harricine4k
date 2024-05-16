8bde5a89b8bc15b81a3cc510b61e3eb4444b0fb8
Renato Torres <renatotorres1@outlook.com>
2024-05-16 20:14:48 -0300

    finalizando header

---
+++ app/components/header/index.tsx
@@ -0,0 +1,35 @@
+'use client'
+
+import Image from "next/image"
+import Link from "next/link"
+import { NavItem } from "./nav-item"
+
+const NavItems = [
+  {
+    label: "Início",
+    href: "/",
+  },
+]
+
+export const Header = () => {
+  return (
+    <header className="absolute top-0 w-full z-10 h-24 flex items-center justify-center bg-red-700 bg-opacity-50">
+      <div className="container flex items-center justify-between">
+        <Link href={"/"}>
+          <Image 
+            width={58}
+            height={49}
+            src={"/images/harricine4k_logo.png"}
+            alt="Logo Harricine4K"
+          />
+        </Link>
+
+        <nav className="flex items-center gap-4 sm:gap-10">
+          {NavItems.map(item => (
+            <NavItem {...item} key={item.label} />
+          ))}
+        </nav>
+      </div>
+    </header>
+  )
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,14 @@
 import { Inter, IBM_Plex_Mono } from 'next/font/google'
 import './globals.css'
 import { ReactNode } from 'react'
+import { Header } from './components/header'
+
+const NavItems = [
+  {
+    label: "Início",
+    href: "/",
+  },
+]
 
 const inter = Inter({
   variable: '--font-inter',
@@ -17,6 +25,7 @@ export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt-BR" className={`${inter.variable} ${plexMono.variable}`}>
       <body>
+        <Header />
         {children}
       </body>
     </html>
