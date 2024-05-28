3b739606fae3ea2a3924c3f77ae844c3322d9d36
Renato Torres <renatotorres1@outlook.com>
2024-05-28 19:19:31 -0300

    finalizando

--- app/components/pages/hero-section/index.tsx
+++ app/components/pages/hero-section/index.tsx
@@ -14,21 +14,22 @@ export const HeroSection = () => {
           whileInView={{ opacity: 1 }}
           exit={{ opacity: 0 }}
         >
-          <p className="font-master text-5xl text-center text-red-600 font-bold stroke-black lg:mt-8 leading-normal">
+          <p className="font-master sm:text-5xl text-xl text-center text-red-600 font-bold stroke-black lg:mt-8 leading-normal">
             O MELHOR SERVIÇO DE STREAMING DO BRASIL DISPONÍVEL PARA VOCÊ!
           </p>
         </motion.div>
       </div>
       <div className="w-full lg:h-[755px] flex flex-col items-center justify-center pt-12 sm:pb-20">
         <Image 
-          width={340}
-          height={327}
+          width={150}
+          height={150}
           src={"/images/harricine4k_logo.png"}
           alt="Logomarca Harricine4K"
+          className="lg:w-[340px] lg:h-[327px]"
         />
       </div>
       <div className="w-full lg:h-[755px] flex flex-col items-center justify-center px-8 py-8 text-center">
-        <p className="font-master text-xl text-neutral-200 font-semibold">
+        <p className="font-master sm:text-3xl text-lg text-neutral-200 font-semibold">
           Confira abaixo todos os nossos serviços e como contratar!
         </p>
       </div>
